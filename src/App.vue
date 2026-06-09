04f8e3a46c8b9d02187942140ac0539e1cc62e3a
Felipe Fama <lipehfama.dev@gmail.com>
2026-06-09 15:31:31 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HeroSection from "./components/HeroSection.vue";
+</script>
+
+<template>
+  <HeroSection />
+</template>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="w-full">
+    <p class="text-2xl">page vue</p>
+  </section>
+</template>
